453d79987d4889b389875a934d39f7583c3da52d
Hänsel <skvortsov.sasha1997@gmail.com>
2023-11-24 16:29:36 +0300

    Lab-5

--- Labs/Lab5/other.cpp
+++ Labs/Lab5/other.cpp
@@ -1,4 +1,6 @@
 #include "other.h"
+#include <iostream>
+
 int ref = 3;
 int Square(int& x) 
 {
@@ -18,6 +20,45 @@ int IncByReference(int& val)
 int IncByPointer(int* val) 
 {
 	return (*val)++;
+}
+
+void Swap_p(int* nX, int* nY) 
+{
+	int temp = *nX;
+	*nX = *nY;
+	*nY = temp;
+}
+
+void Swap_r(int& nX, int& nY)
+{
+	int temp = nX;
+	nX = nY;
+	nY = temp;
+}
+
+const int N = 5;
+
+void PrintArray(int* ar, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << ar[i];
+	}
+}
+
+int Min(int* ar, int size) 
+{
+	int min = ar[0];
+
+	for (int i = 1; i < size; i++)
+	{
+		if (ar[i] < min)
+		{
+			min = ar[i];
+		}
+	}
+
+	std::cout << min;
 }
 
 
--- Labs/Lab5/other.h
+++ Labs/Lab5/other.h
@@ -5,4 +5,8 @@ extern int Square(int& x);
 extern int IncByValue(int val);
 extern int IncByReference(int& val);
 extern int IncByPointer(int* val);
-
+extern void Swap_p(int* nX, int* nY);
+extern void Swap_r(int& nX, int& nY);
+extern const int N;
+extern void PrintArray(int* ar, int size);
+extern int Min(int* ar, int size);
